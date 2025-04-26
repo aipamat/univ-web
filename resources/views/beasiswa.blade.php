8fe5987ecb10f9f931ea4da611befc84c2390cd4
Arshi <fahrezzi@widianto.org>
2025-04-26 11:23:38 +0700

    Changes: Pada Page Beasiswa, mengubah yang awalnya section menjadi wrapper img background, di ubah menjadi div atau container yang berbeda

--- resources/views/beasiswa.blade.php
+++ resources/views/beasiswa.blade.php
@@ -3,22 +3,27 @@
 
 @section('content')
     <!-- start page title -->
-    <section
-        class="page-title-separate-breadcrumbs cover-background border-top border-4 border-color-base-color top-space-margin"
-        style="background-image: url('{{Storage::url($beasiswaUtamaItems->banner)}}')">
-        <div class="opacity-full bg-gradient-dark-transparent"></div>
-        <div class="container position-relative">
-            <div class="row align-items-start align-items-lg-end justify-content-end flex-column flex-lg-row extra-small-screen">
-                <div class="col-xxl-7 col-lg-6 col-md-10 position-relative page-title-large md-mb-10px xs-mb-5px">
+    <section class="tw-relative tw-w-full tw-h-[600px] md:tw-h-[500px] border-top border-4 border-color-base-color top-space-margin overflow-hidden">
+        <!-- Gambar belakang -->
+        <img src="{{ Storage::url($beasiswaUtamaItems->banner) }}"
+             alt="Banner"
+             class="tw-absolute tw-top-0 tw-left-0 tw-w-full tw-h-[500px] md:tw-h-[600px] tw-object-cover tw-z-0" />
+    
+        <!-- Overlay gelap -->
+        <div class="tw-absolute tw-top-0 tw-left-0 tw-w-full tw-h-full tw-bg-gradient-to-b tw-from-black/50 tw-to-black/50 tw-z-10"></div>
+    
+        <!-- Konten -->
+        <div class="container tw-relative tw-z-20 tw-h-full tw-flex tw-flex-col tw-justify-end md:tw-justify-center">
+            <div class="row align-items-start align-items-lg-end justify-content-end flex-column flex-lg-row extra-small-screen tw-h-full">
+                <div class="col-xxl-7 col-lg-6 col-md-10 page-title-large md-mb-10px xs-mb-5px">
                     <h1 class="text-white alt-font fw-500 ls-minus-2px mb-0">Beasiswa</h1>
                 </div>
                 <div class="col-xxl-5 col-lg-6 col-md-10 last-paragraph-no-margin">
-                    <p class="fs-20 text-white opacity-7 md-w-80 sm-w-100">
-                    </p>
+                    <p class="fs-20 text-white opacity-7 md-w-80 sm-w-100"></p>
                 </div>
             </div>
         </div>
-    </section>
+    </section>    
     <!-- end page title -->
 
     <!-- start section -->
